a7b985e25edf4be22c84d8fca9f4fca4af9e56d7
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-06-20 14:25:29 -0400

    first commit

---
+++ Restaurant Faves Lab/Controllers/OrderController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant_Faves_Lab.Models;
+
+namespace Restaurant_Faves_Lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        RestaurantDbContext dBContext = new RestaurantDbContext();
+
+        //api/Order
+        [HttpGet()]
+        public IActionResult GetAll(string? restaurant=null, bool? orderAgain = null)
+        {
+            List<Order> result = dBContext.Orders.ToList();
+
+            if (restaurant != null)
+            {
+                result= result.Where(x=> x.Restaurant.ToLower().Contains(restaurant)).ToList();
+            }
+            if (orderAgain != null)
+            {
+                result = result.Where(x=> x.OrderAgain == orderAgain).ToList();
+            }
+
+            return Ok(result);
+        }
+
+        //api/Order/2
+        [HttpGet("{id}")]
+
+        public IActionResult GetID(int id)
+        {
+            Order result = dBContext.Orders.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        //api/Order
+        [HttpPost()]
+        public IActionResult AddOrder([FromBody]Order newOrder)
+        {
+            newOrder.Id = 0;
+            dBContext.Orders.Add(newOrder);
+            dBContext.SaveChanges();
+            return Created($"/api/Order/{newOrder.Id}", newOrder);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            Order result = dBContext.Orders.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            dBContext.Orders.Remove(result);
+            dBContext.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook([FromBody] Order targetOrder, int id)
+        {
+            if(targetOrder.Id != id)
+            {
+                return BadRequest();
+            }
+            if(!dBContext.Orders.Any(order=> order.Id == id)){
+                return NotFound();
+            }
+            dBContext.Orders.Update(targetOrder);
+            dBContext.SaveChanges();
+            return NoContent();
+        }
+    }
+}
